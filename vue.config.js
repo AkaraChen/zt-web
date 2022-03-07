91b51d39ff78b586429ff3c542962c9658fcadc5
AkaraChen <akarachen@outlook.com>
2022-03-07 23:18:53 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     pwa: {
       serviceWorker:false,
-      workboxPluginMode:'InjectManifest',
-      workboxOptions: {
-        swSrc: './app/sw.js', /* Empty file. */
-      }
     }
   }
