a20e1b2c94f7c8113d8d47660ef7896d7b2edf16
AkaraChen <akarachen@outlook.com>
2022-03-07 23:18:02 +0800

    Create vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pwa: {
+      serviceWorker:false,
+      workboxPluginMode:'InjectManifest',
+      workboxOptions: {
+        swSrc: './app/sw.js', /* Empty file. */
+      }
+    }
+  }
